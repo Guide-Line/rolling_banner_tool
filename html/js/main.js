c7293b945e005f7436f62998b89d1f25c6d336b8
EBAYKOREA\jangdk <jangdonggook@outlook.com>
2017-08-22 11:20:41 +0900

    창화면 리사이즈 작업 중에 있습니다.

--- html/js/main.js
+++ html/js/main.js
@@ -5,30 +5,47 @@
 
 function RollingBanner(){
     console.log("생성자 함수 () => RollingBanner");
+   	this.isWeb = true; // pc 화면일때  true  , mobile 화면일때 false
+
 };
 
 RollingBanner.prototype.init = function(){
 	console.log("기본실행 () => init");
-	this.btnEvent();
+	this.btnEvent();	
 };
 
 RollingBanner.prototype.btnEvent = function(){
 	console.log("이벤르틑 발생시키는 버튼 () = > btnEvent");
+	var _this = this;
 	var isView = 0;
 
     $(".icon li>a").each(function(index) {
         var $this = $(this)
         $this.click(function() {
-            isView = index;            
-            selectMenu(isView)
+            isView = index;
+            if (isView == 0) {            	
+                _this.isWeb = true;
+                console.log("desktop" , _this.isWeb)
+            } else {            	
+                _this.isWeb = false;
+                console.log("mobile" , _this.isWeb)
+            };            
+            selectMenu(isView);
         });
     });
     selectMenu = function(n) {
         $(".icon li").removeClass("select");
         $(".icon li").eq(n).addClass("select");
+        if(n == 0){
+        	console.log("desktop select");
+			_this.contentsResize(980)
+        }else{
+        	console.log("mobile select")
+        	_this.contentsResize(375)
+        }
     };
 
-    $(".downBtn").bind('click', function() {
+    $(".downBtn").bind('click', function(e) {
 		e.preventDefault();
     });
     $('.viewBtn').bind("click", function(e) {
@@ -37,4 +54,12 @@ RollingBanner.prototype.btnEvent = function(){
     });
 };
 
+RollingBanner.prototype.contentsResize = function(stageWidth){
+	console.log("보여지는 화면 리사이즈 () = > contentsResize");
 
+	$("#mobileViewArea").stop(true, true).animate({
+        width: stageWidth
+    }, 200, function() {
+        //ui_update();
+    });
+}
